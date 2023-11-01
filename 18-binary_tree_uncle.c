da585d426069ac546678ea69aeeb9156f501a479
ebhenhaazer <en.mwathy@gmail.com>
2023-11-01 06:03:26 -0700

    18. Uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - find if node is sibling
+ * @node: check node
+ * Return: The uncle node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !(node->parent) || !(node->parent->parent))
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
