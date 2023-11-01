59c8a404ea62e5d0f93cf3fdcf42d4c6e06f157a
ebhenhaazer <en.mwathy@gmail.com>
2023-11-01 13:42:26 -0700

    15. Is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - if tree is full or not
+ * full is if with tow or no child
+ * @tree: check tree
+ * Return: 1 if is full, 0 if not
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int r = 0, l = 0;
+
+	if (!tree)
+	{
+		if (tree->r && tree->l)
+		{
+			r = binary_tree_is_full(tree->r);
+			l = binary_tree_is_full(tree->l);
+			if (r == 0 || l == 0)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		else if (tree->r == NULL && tree->l == NULL)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
