b85c820fc7867bf18f0fe2499649d494b39977c4
ebhenhaazer <en.mwathy@gmail.com>
2023-11-01 05:05:44 -0700

    5. Is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,11 +5,9 @@
  * @node: node to check
  * Return: 1 if leaf, 0 if not
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node)
+	if (!node || !(node->parent))
 		return (0);
-	if (!(node->left) && !(node->right))
-		return (1);
-	return (0);
+	return (1);
 }
