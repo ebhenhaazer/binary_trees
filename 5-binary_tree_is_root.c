091e33d9d184be1a9a813b8be6f7799ce6fd0d7c
ebhenhaazer <en.mwathy@gmail.com>
2023-11-01 05:13:10 -0700

    coorections 1

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - check if leaf
+ * binary_tree_is_root - check if node is root
  * @node: node to check
- * Return: 1 if leaf, 0 if not
+ * Return: 1 if root, 0 if not
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node || !(node->parent))
+	if (!(node) || !(node->parent))
 		return (0);
 	return (1);
 }
